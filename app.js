6afdef74c48e7b8fdee764882f4f84025df314c9
urmil22 <urmilbhavsar8@gmail.com>
2021-07-21 14:25:19 +0530

    error fixed

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
-var outputDiv = document.querySelector("#output-div");
+var outputDiv = document.querySelector("#output");
 
 //var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
 
