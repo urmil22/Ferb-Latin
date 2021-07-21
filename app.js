67d8f8a95582106d78c5446b91a259bca43650c6
urmil22 <urmilbhavsar8@gmail.com>
2021-07-21 10:56:47 +0530

    added style and image

---
+++ app.js
@@ -0,0 +1,29 @@
+var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output-div");
+
+//var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+
+var serverURL = "https://api.funtranslations.com/translate/ferb-latin.json"
+
+
+
+function getTranslationURL(input) {
+    return serverURL + "?" + "text=" + input;
+}
+
+
+function clickHandler() {
+
+    var inputText = txtInput.value;
+
+    fetch(getTranslationURL(inputText))
+        .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            outputDiv.innerText = translatedText;
+        });
+}
+
+
+btnTranslate.addEventListener("click", clickHandler)
